5e3db9be03ed32fdff654013248f7fb881040b64
jamesli12 <leej55064@gmail.com>
2023-10-12 16:26:30 -0400

    Removing unused imports

--- carbon/src/main/java/com/allsaints/carbon/CarbonController.java
+++ carbon/src/main/java/com/allsaints/carbon/CarbonController.java
@@ -1,15 +1,11 @@
 package com.allsaints.carbon;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(maxAge = 3600)
