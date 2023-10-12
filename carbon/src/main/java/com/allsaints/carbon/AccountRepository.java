8353dfd5b40135d4616a44c1735d8cb7b517e462
jamesli12 <leej55064@gmail.com>
2023-10-12 16:24:38 -0400

    Whoopsie

--- carbon/src/main/java/com/allsaints/carbon/AccountRepository.java
+++ carbon/src/main/java/com/allsaints/carbon/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.allsaints.carbon;
 
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -8,4 +10,7 @@ public interface AccountRepository extends MongoRepository<Account, String> {
     @Query ("{'email': ?0}")
     Account findByEmail(String email);
     public long count();
+
+    @Query("{'lastName': ?0}")
+    List<Account> findByLastName(String lastName);
 }
