e63f41f3f5277e7268ff62e54a68422dc41772dd
jamesli12 <leej55064@gmail.com>
2023-10-12 17:52:35 -0400

    Add Find By Email Tests

--- carbon/src/test/java/com/allsaints/carbon/CarbonApplicationTests.java
+++ carbon/src/test/java/com/allsaints/carbon/CarbonApplicationTests.java
@@ -1,13 +1,37 @@
 package com.allsaints.carbon;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarbonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private AccountRepository repository;
 
+	@Test
+	void testFindByEmail() {
+		// Generate a random email
+		String email = UUID.randomUUID().toString() + "@example.com";
+
+		// Create a new account
+		Account account = new Account();
+		account.email = email;
+		account.password = "password";
+		account.firstName = "John";
+		account.lastName = "Doe";
+		repository.save(account);
+
+		// Test the findByEmail function
+		Account foundAccount = repository.findByEmail(email);
+		assertNotNull(foundAccount);
+		assertEquals("John", foundAccount.firstName);
+		assertEquals("Doe", foundAccount.lastName);
+	}
 }
